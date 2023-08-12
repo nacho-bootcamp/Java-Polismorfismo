f685025ab55117540ec4c960c870eaa0052e4b34
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-08-11 23:57:21 -0300

    polimorfismo intro

---
+++ TestReferencia.java
@@ -0,0 +1,11 @@
+public class TestReferencia {
+  public static void main(String[] args) {
+    Funcionario funcionario = new Funcionario();
+    funcionario.setNombre("nacho");
+    funcionario.setSalario(2334);
+
+    Gerente gerente = new Gerente();
+    gerente.setNombre("nacho1");
+    gerente.setSalario(700000);
+  }
+}
